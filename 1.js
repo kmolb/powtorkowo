0b2af44b45f1f19e2d6364ece497641eaf30924c
Kamil Olber <olb.kamil@gmail.com>
2019-01-12 09:12:17 +0100

    promisy v2

--- 1.js
+++ 1.js
@@ -2,19 +2,28 @@
 
 const prom = () => {
     return new Promise((resolve, reject) => {
-        resolve(true);
-        setTimeout(() => {
-            resolve(true);
-           // console.log('from set timeout');
-        }, 4000);
+          setTimeout(() => {
+            resolve(1);
+        }, 1000);
     })
 }
 
+const prom2 = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(2);
+        }, 1000);
+    })
+}
 prom()
     .then((resp) => {
      console.log(resp);
      throw Error;
-})
+
+    })
+    .then((resp) => {
+        console.log(resp);
+    })
     .catch((err) => {
-     console.log(err)
+     console.log('error', err)
     });
